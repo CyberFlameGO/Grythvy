05b32093299a7fb86d387b3b7d646150757bde76
CyberFlame <cyberflameu@gmail.com>
2024-05-21 13:55:47 +1200

    should fix yo merge issues yo

--- src/main/java/net/cyberflame/grythvy/BotConfig.java
+++ src/main/java/net/cyberflame/grythvy/BotConfig.java
@@ -1,23 +1,8 @@
-/*
- * Copyright 2018 John Grosh (jagrosh)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.jagrosh.jmusicbot;
+package net.cyberflame.grythvy;
 
-import com.jagrosh.jmusicbot.entities.Prompt;
-import com.jagrosh.jmusicbot.utils.FormatUtil;
-import com.jagrosh.jmusicbot.utils.OtherUtil;
+import net.cyberflame.grythvy.entities.Prompt;
+import net.cyberflame.grythvy.utils.OtherUtil;
+import net.cyberflame.grythvy.utils.TimeUtil;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.typesafe.config.*;
 import java.io.IOException;
